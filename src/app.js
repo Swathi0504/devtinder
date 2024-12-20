410844ff23b0ecf044f227bb760bdf27f36afcd8
Swathi0504 <swathisivakumarbecse@gmail.com>
2024-12-20 10:42:29 +0530

    Schema

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.post("/signup", async (req,res)=>{
 
-    console.log(req.body);
+   console.log(req.body);
     
    const user = new User(req.body)
     
@@ -17,8 +17,57 @@ app.post("/signup", async (req,res)=>{
         res.send("User Successfully Saved");
    }
    catch(err) {
-       res.send("Unable to save the data");
+       res.send(err);
    }
+})
+
+
+//getting user data by email address\
+app.get("/finduser",async (req,res)=>{
+    try {
+        const user = await User.find({"emailId": req.body.emailId});
+        if(user.length===0) {
+            res.status(404).send("User not found");
+        }
+        res.send(user);
+    }
+    catch(err) {
+       res.status(400).send("Something went wrong");
+    }
+})
+
+//getting all users
+app.get("/feed",async (req,res)=>{
+     try {
+        const users = await User.find({});
+        res.send(users);
+     }
+     catch(err) {
+        res.status(404).send("Something went wrong");
+     }
+})
+
+app.delete("/user",async(req,res)=>{
+    try {
+       const user=await User.findByIdAndDelete({_id:req.body.id});
+       res.send("User deleted successfully")
+    }catch(err) {
+       res.status(400).send("Unable to delete"); 
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const data = req.body;
+    try {
+       const user=await User.findByIdAndUpdate({_id:req.body.id},data,{
+        returnDocument:"before",
+        runValidators:"true"
+       });
+       console.log(user);
+       res.send("User updated successfully")
+    }catch(err) {
+       res.status(400).send("Something went wrong"+ err.message); 
+    }
 })
 
 connectDb().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,58 @@
 
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
     firstName : {
-        type : String
+        type : String,
+        required: true,
+        minLength: 4,
+        maxLength: 100,
     },
     lastName : {
         type : String
     },
     emailId : {
-        type : String
+        type : String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password : {
-        type : String
+        type : String,
+        required: true,
     },
     age : {
-        type : Number
+        type : Number,
+        min : 18,
     },
     gender : {
-         type : String
+         type : String,
+         lowercase: true,
+         validate(value) {
+           if(!["male","female","others"].includes(value)) {
+               throw new Error("Gender data is not valid");
+               
+           }
+         }
+    },
+    photoUrl : {
+        type : String,
+        default:"https://pinnacle.works/wp-content/uploads/2022/06/dummy-image.jpg"
+    },
+    about : {
+        type : String,
+        default: "This is a default about for the user"
+    },
+    skills : {
+        type : [String]
     }
-});
+},
+{
+    timestamps: true
+}
+);
 
 
 const User = mongoose.model("User",userSchema);
